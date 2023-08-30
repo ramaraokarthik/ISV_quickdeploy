03562d6bfc1f4e0d291c4ce0b11960e649270bf2
Karthik Rama Rao <142759824+ramaraokarthik@users.noreply.github.com>
2023-08-30 19:08:41 +0800

    Update gitlab.sh

--- gitlab.sh
+++ gitlab.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-echo Open your cloud console. Click the hamburger icon.
-echo Hover one  "Compute Engine"  and then click VM Instances
-echo Look for for the vm name that you gave when you subscribed 
-echo for gitlab. If default it will be of the form 
-echo gitlab-vm-x-vm ( x will be a numeral)
-echo Copy the "External IP" and respond to the following question
-echo Incase you have associated that IP to a domain name, give the
-echo domain name as the answer to the followign question 
+echo "Open your cloud console. Click the hamburger icon."
+echo "Hover one  "Compute Engine"  and then click VM Instances"
+echo "Look for for the vm name that you gave when you subscribed"
+echo "for gitlab. If default it will be of the form"
+echo "gitlab-vm-x-vm ( x will be a numeral)"
+echo "Copy the "External IP" and respond to the following question"
+echo "Incase you have associated that IP to a domain name, give the"
+echo "domain name as the answer to the followign question"
 
 read -p "Do you have a domain associated with the IP (repond with only y or n) : " domn
 read -p "Enter the IP address of the server or associated domain name : " addr
