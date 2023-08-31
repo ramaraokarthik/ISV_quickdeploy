0fbb0db3d33ffef0b3bd1296d2c8fd3fa4c0b3bc
Karthik Rama Rao <142759824+ramaraokarthik@users.noreply.github.com>
2023-08-31 12:33:00 +0800

    Update gitlab.sh

--- gitlab.sh
+++ gitlab.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+BOLD=$(tput bold)
+NORM=$(tput sgr0)
 
+echo -e "${Red}${BOLD} APAC ISV QuickDeploy for Gitlab"
 
-echo "Open your cloud console. Click the hamburger icon."
+echo -e "${GREEN}${BOLD} Open your cloud console. Click the hamburger icon."
 echo "Hover one  "Compute Engine"  and then click VM Instances"
 echo "Look for for the vm name that you gave when you subscribed"
 echo "for gitlab. If default it will be of the form"
 echo "gitlab-vm-x-vm ( x will be a numeral)"
 echo "Copy the "External IP" and respond to the following question"
 echo "Incase you have associated that IP to a domain name, give the"
-echo "domain name as the answer to the followign question"
+echo -e  "domain name as the answer to the followign question ${NC}${NORM}"
 
 read -p "Do you have a domain associated with the IP (repond with only y or n) : " domn
 read -p "Enter the IP address of the server or associated domain name : " addr
@@ -18,11 +24,7 @@ sudo apt-get install -y postfix
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
 if  [ "$domn" == "y" ]
 then
- echo $domn
  sudo EXTERNAL_URL="https:gitlab.$addr" apt-get install gitlab-ee
- echo "Helloo difrt" 
-else i[ "$domn" == "n"] 
- echo " second $domn"
+else [ "$domn" == "n"] 
  sudo EXTERNAL_URL="http:$addr" apt-get install gitlab-ee
- echo "Heloo Seond"
 fi
