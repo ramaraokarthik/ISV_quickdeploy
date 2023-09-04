96e38d6dd089c5054a8b0e12c5ae5062c79fee79
Karthik Rama Rao <142759824+ramaraokarthik@users.noreply.github.com>
2023-09-04 19:54:23 +0800

    Update gitlab.sh

--- gitlab.sh
+++ gitlab.sh
@@ -60,4 +60,4 @@ pass=sudo awk '$1=="Password:"{print $2}' /etc/gitlab/initial_root_password
 echo "##!! Open the browser and connect to the gitlab instance with the IP address or gitlab.<domain name given above>"
 echo "##!! Login with the root credentals below and change password immediately. Add uses and administer the instance"
 echo "##!! Login name : root"
-echo "##!! Password: $pass ( change after loggin in. Will be valid for 24hrs from inital installation only) "
+echo -e "##!! Password: $pass ( change after loggin in. Will be valid for 24hrs from inital installation only) "
